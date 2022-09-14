ea1ac28fab38b5fde283d2a0cbcb272f4f6abeab
Philipp 0xf3f <7755130+0xf3f@users.noreply.github.com>
2022-09-14 16:03:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vite - React - Typescript - TailwindCSS --- Starter
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <span className="text-3xl underline">Hello World</span>
+    </div>
+  );
+}
+
+export default App;
